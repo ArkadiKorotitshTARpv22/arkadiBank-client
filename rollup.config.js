1236bd15212bd8e3ae0ea3d30ce19e64b3933762
ArkadiKorotitshTARpv22 <gentletin62@gmail.com>
2024-10-10 13:55:06 +0300

    test 1

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
-import copy from "rollup-plugin-copy";
+import copy from 'rollup-plugin-copy';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
@@ -24,21 +24,22 @@ export default {
 		input: config.client.input(),
 		output: config.client.output(),
 		plugins: [
+			copy({
+				targets: [{
+					src: 'node_modules/bootstrap/dist/**/*',
+					dest: 'static/vendor/bootstrap'
+				},
+				{
+					src: 'node_modules/jquery/dist/**/*',
+					dest: 'static/vendor/jquery'
+				}]
+			}),
 			replace({
 				preventAssignment: true,
 				values:{
 					'process.browser': true,
 					'process.env.NODE_ENV': JSON.stringify(mode)
 				},
-			}),
-			copy({
-				targets: [{
-					src: 'node_modules/bootstrap/dist/**/*',
-					dest: 'static/vendor/bootstrap'
-				},{
-					src: 'node_modules/jquery/dist/**/*',
-					dest: 'static/vendor/bootstrap'
-				}]
 			}),
 			svelte({
 				compilerOptions: {
